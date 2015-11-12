6d9ecce45caa384b7a5c326bbccd590c52b57559
Jayachandra <jjayacha@wm.com>
2015-11-12 18:30:03 +0530

    ui fixes

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('clientApp', ['appRoute', 'authService', 'mainCtrl', 'calendarService', 'empCtrl', 'createLeaveCtrl', 'mwl.calendar', 'ui.bootstrap', 'ngTable', 'recordsService', 'viewRecordsCtrl', 'managerCtrl', 
+var app = angular.module('clientApp', ['appRoute', 'authService', 'mainCtrl', 'calendarService', 'empCtrl', 'createLeaveCtrl', 'wt.responsive','mwl.calendar', 'ui.bootstrap', 'ngTable', 'recordsService', 'viewRecordsCtrl', 'managerCtrl', 
 	'managerService', 'createEmployeeCtrl', 'dataService' , 'approvalRequestCtrl', 'leaveService', 'forgotPasswordCtrl', 'changePasswordCtrl', 'validation.match', 'editLeaveRecordCtrl', 'viewLeaveRecordCtrl']);
 /*
 app.config(function($httpProvider) {
